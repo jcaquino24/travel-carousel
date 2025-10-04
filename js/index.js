4fcfb102802877e430b41973a2a4435af4a89dcc
clifford aquino <36370796+jcaquino24@users.noreply.github.com>
2025-10-04 23:45:09 +0800

    Final

--- js/index.js
+++ js/index.js
@@ -1,27 +1,23 @@
-
-
-<script>
-  $(document).ready(function () {
-    function toggleCarousel() {
-      if ($(window).width() < 900) {
-        if (!$(".destinations").hasClass("slick-initialized")) {
-          $(".destinations").slick({
-            dots: true,
-            arrows: false,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          });
-        }
-      } else {
-        if ($(".destinations").hasClass("slick-initialized")) {
-          $(".destinations").slick("unslick");
-        }
+$(document).ready(function () {
+  function toggleCarousel() {
+    if ($(window).width() < 900) {
+      if (!$(".destinations").hasClass("slick-initialized")) {
+        $(".destinations").slick({
+          dots: true,
+          arrows: false,
+          infinite: true,
+          speed: 500,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        });
+      }
+    } else {
+      if ($(".destinations").hasClass("slick-initialized")) {
+        $(".destinations").slick("unslick");
       }
     }
+  }
 
-    toggleCarousel();
-    $(window).on("resize", toggleCarousel);
-  });
-</script>
+  toggleCarousel();
+  $(window).on("resize", toggleCarousel);
+});
